4bde4579e718611e730f6b4bbd62297b2272f88a
ce.miyashita <ce.miyashita@plusism.dev>
2024-02-18 22:10:04 +0900

    Quotationモデルの実装

---
+++ src/Excel.Report.PDF.BlazorWebApp/Models/Quotation/Quotation.cs
@@ -0,0 +1,18 @@
+﻿namespace Excel.Report.PDF.BlazorWebApp.Models;
+
+public class Quotation
+{
+	public string Title { get; set; } = string.Empty;
+
+	public string Client { get; set; } = string.Empty;
+
+	public string PersonInCharge { get; set; } = string.Empty;
+
+	public List<QuotationDetail> Details { get; } = new();
+
+	public decimal Total => Details.Sum(x => x.Total);
+
+	public decimal Tax => Total * (decimal)0.1;
+
+	public decimal TotalInTax => Total + Tax;
+}
---
+++ src/Excel.Report.PDF.BlazorWebApp/Models/Quotation/QuotationDetail.cs
@@ -0,0 +1,14 @@
+﻿namespace Excel.Report.PDF.BlazorWebApp.Models;
+
+public class QuotationDetail
+{
+	public string Title { get; set; } = string.Empty;
+
+	public string Detail { get; set; } = string.Empty;
+
+	public decimal Price { get; set; }
+
+	public decimal Discount { get; set; }
+
+	public decimal Total => Price - Discount;
+}
